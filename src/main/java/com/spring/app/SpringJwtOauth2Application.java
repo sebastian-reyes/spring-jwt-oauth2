03cd3afeb154370952649097101ac87fd763c3b7
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-08-24 13:25:25 -0500

    Creando proyecto Spring, inicio del repo

---
+++ src/main/java/com/spring/app/SpringJwtOauth2Application.java
@@ -0,0 +1,13 @@
+package com.spring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/app/SpringJwtOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
