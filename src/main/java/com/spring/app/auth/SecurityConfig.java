aa61d57aebdde98cd9c30735aeec0d20174685c5
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-09-06 12:33:28 -0500

    Añadiendo configuración para el servidor de autorización

--- src/main/java/com/spring/app/auth/SecurityConfig.java
+++ src/main/java/com/spring/app/auth/SecurityConfig.java
@@ -3,11 +3,11 @@ package com.spring.app.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
@@ -23,5 +23,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.userDetailsService(uService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception{
+        return super.authenticationManager();
     }
 }
