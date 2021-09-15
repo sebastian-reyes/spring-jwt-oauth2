a4d1b5632bb4266fc065250ce1896d49eaf47d72
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-09-14 20:56:29 -0500

    Añadiendo permisos para CORS

--- src/main/java/com/spring/app/rest/UsuarioRestController.java
+++ src/main/java/com/spring/app/rest/UsuarioRestController.java
@@ -12,6 +12,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"*"})
 @RequestMapping("/api/usuarios")
 public class UsuarioRestController {
 
